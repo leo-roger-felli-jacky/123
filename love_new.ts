4c0a23eaeba5668190f2e32d6100c19774b44857
felli0701 <127929888+felli0701@users.noreply.github.com>
2023-03-16 16:00:16 +0800

    更新 pxt.json, main.blocks, main.ts, love_new.ts

--- love_new.ts
+++ love_new.ts
@@ -1,12 +1,12 @@
 // 在這裡添加你的程式
 //%color="#FFAB19"weight=100 icon="\uf08c"
-namespace自訂文字{
+namespace 自訂文字 {
     //%block="顯示 | 文字%text | 顏色 %fg | 背景 %bg"
     //% fg.min=0 fg.max=15 bg.min=0 bg.max=15
     //% text.defl="Hello"
     export function 顯示文字(text: string,fg: number,bg: number){
         const font = image.font8;
-        const width = font.charWidth * text.langth;
+        const width = font.charWidth * text.length;
         const height = font.charHeight;
         const res = image.create(width, height);
         res.fill(bg);
